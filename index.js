a309d25fca0c5dd5bc2f764806b3d245a8169a7e
Daniel McSheehy <dsm140130@utdallas.edu>
2018-02-20 21:49:30 -0600

    Added bunch of stuff

--- index.js
+++ index.js
@@ -16,73 +16,57 @@ db.loadDatabase(function (err) {    // Callback is optional
 r.getHot().then(listing => {
 
   var posts = listing.map(submission => submission);
-  
   var content = [];
-
   var db = {};
   var str = "";
-
   posts.forEach(function(p) {
+		p.expandReplies({limit: 2, depth: 1}).then(obj => { // depth: parent, limit: branches
+				//console.log('post: ', p.id);
+				var title = obj.title;
 
-	p.expandReplies({limit: 1, depth: 1}).then(obj => {
-	  	//console.log('post: ', p.id);
-	  	var title = obj.title;
-	  	
-	  	var replies = obj.comments.map(function(comments){ // TODO: username
-	  	 return {'body': comments.body};
-	  	});
-
-			var data = {
-				'_id': p.id, 
-				'title':title, 
-				'name': p.name,
-				'thumbnail': p.thumbnail, 
-				'url': p.url, 
-				'replies': replies, 
-				'score': p.score,
-				'num_comments': p.num_comments,
-				'subreddit': p.subreddit_name_prefixed
-			};
-	  	storeDocument(data); ///////////////// 
-	  	
-	});
+				var replies = obj.comments.map(function(comments){ // TODO: username
+					return {'body': comments.body, 'name': comments.name, 'score': comments.score, 'replies': comments.replies};
+				});
+				console.log(replies);
+				
+				var data = {
+					'_id': p.id, 
+					'title':title, 
+					'name': p.name,
+					'thumbnail': p.thumbnail, 
+					'url': p.url, 
+					'replies': replies, 
+					'score': p.score,
+					'num_comments': p.num_comments,
+					'subreddit': p.subreddit_name_prefixed
+				};
+				storeDocument(data); ///////////////// 
+				
+		});
   });
   getPosts();
 });
 
 function build(_id, title_text, thumbnail_url, post_url, subreddit, num_comments) {
-
-
+ // <div class="comment_tag"> ${num_comments}</div>
 	let html_text = `<div class="post_wrapper">
       <img class="thumbnail" src=${thumbnail_url} alt="Smiley face">
       <div class="title">${title_text}</div>
       <div class="subreddit_tag">${subreddit}</div>
-      <div class="comment_tag"> ${num_comments}</div>
+      <a href="comments.html?postId=${_id}" class="comment_tag">${num_comments}</a>
       <hr class="post_hr">
     </div>`;
 
-
     var ele = document.getElementById('content_box');
     ele.innerHTML += html_text;
-    //post_wrapper.appendChild(ele);
-    // console.log(document.getElementById('content_box'));
-
-
 }
 
 function load(offlineContentData) {
-	
 	for (i = offlineContentData.length -1; i--;) {
 		let post = offlineContentData[i];
 		build(post._id, post.title, post.thumbnail, post.url, post.subreddit, post.num_comments);
+		//console.log(post);
 	}
-
-
-
-	// offlineContentData.forEach(function(post){
-	// 	build(post._id, post.title, post.thumbnail, post.url, post.subreddit, post.num_comments); // TODO: NEED TO ADD KARMA SUBREDDIT ECT
-	// });
-	//console.log(offlineContentData);
 }
 
 function storeDocument(postObj) {
@@ -92,14 +76,9 @@ function storeDocument(postObj) {
       });
 } 
 
-function checkDuplicate(_post_id) {
-	//TODO: 
-}
-
 function getPosts(key) {
- //key?
+  //key?
 	db.find({ }, function (err, docs) {
 		load(docs);
 	});
-	    
 }
--- replies.js
+++ replies.js
@@ -1,37 +1,43 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/Magmadb";
 
-var _post_id = window.location.href.split("file:///Users/dsm/code/electron/Magma/comments.html?commentId=")[1];
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: '/Users/dsm/code/electron/Magma/eleStorage.db' });
+db.loadDatabase(function (err) {    // Callback is optional
+  // Now commands will be executed
+});
+
+var _post_id = window.location.href.split("file:///Users/dsm/code/electron/Magma/comments.html?postId=")[1];
 getPosts(_post_id);
 
 function getPosts(_post_id) {
-	MongoClient.connect(url, function(err, db) {
-	  if (err) throw err;
-	  var dbo = db.db("Magmadb");
+	
 
 	var query = {_id: _post_id};
-	dbo.collection("posts").find(query).toArray(function(err, result) { // EVERYTHING!!!
-	    if (err) throw err;
-	    var _post = result[0];
-	    _post.replies.forEach(function(comments){
-	    	build(comments.body);
-	    	console.log(comments.body);
-	    });
-	    
-	    
-	    //return result;
-	    db.close();
-	  });
+	console.log(query);
+
+	db.find(query, function (err, docs) {
+		console.log(docs);
+		generateComments(docs[0]);
 	});
 }
-function build(body_text) {
 
-	var element = document.createElement('div');
-	element.className = "object";
-	element.textContent = body_text;
-	
-	document.getElementsByTagName('body')[0].appendChild(element);
+function generateComments(postObj) { // TODO: Names
+	postObj.replies.forEach(function(commentObj){ 
+		build(commentObj.body,0);
+		commentObj.replies.forEach(function(subObj){
+			build(subObj.body, 1);
+			subObj.replies.forEach(function(lastObj){
+				build(lastObj.body, 2);
+			})
+		});
+	});
 }
-
+function build(body, level){
+  let left_margin = `margin-left:${level*20}px;`;
+  let html_text = `<div class="post_wrapper">
+                    <div class="added" style=${left_margin}>${body}</div>
+                   </div>`;
+  var ele = document.getElementById('content_box');
+    ele.innerHTML += html_text;
+}
 
 //var data = {'_id':p.id, 'title':title, 'name': p.name, 'thumbnail': p.thumbnail, 'replies': replies, 'subreddit': p.subreddit_name_prefixed, 'url': p.url, 'score:' p.score};
